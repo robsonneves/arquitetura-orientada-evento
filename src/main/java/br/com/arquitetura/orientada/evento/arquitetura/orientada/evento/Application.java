92a179f3376605c7434c9c8035581ead5263e3f3
robsonneves <robsonnevesrodrigues@yahoo.com.br>
2022-11-08 20:55:57 -0300

    add projeto base initializer spring-boot

---
+++ src/main/java/br/com/arquitetura/orientada/evento/arquitetura/orientada/evento/Application.java
@@ -0,0 +1,13 @@
+package br.com.arquitetura.orientada.evento.arquitetura.orientada.evento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
